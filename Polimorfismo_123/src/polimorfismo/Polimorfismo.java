c4268036081db3c6cde6a441193a88b1c580184c
Cristian3214 <L21550325@chihuahua2.tecnm.mx>
2022-04-29 09:26:44 -0600

    Polimorfismo Proyecto Clase1

--- Polimorfismo_123/src/polimorfismo/Polimorfismo.java
+++ Polimorfismo_123/src/polimorfismo/Polimorfismo.java
@@ -5,6 +5,7 @@
  */
 package polimorfismo;
 
+import POL.*;
 
 /**
  *
@@ -16,8 +17,15 @@ public class Polimorfismo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         
-       }
+        EmpleadoAsalariado empleadoAsalariado = new EmpleadoAsalariado("Daira", "Rodrigues", "111", 800.00);
+        EmpleadoBaseMasComision empleadoBaseMasComision = new EmpleadoBaseMasComision("Jorge", "Rodrigues", "511", 800.00, .95, 400);
+        EmpleadoPorComision empleadoPorComision = new EmpleadoPorComision("Pepe", "Octaz", "231", 800, 600.00);
+        EmpleadoPorHoras empleadoPorHoras = new EmpleadoPorHoras("Daiel", "Jaquez", "1234", 16.75, 40);
+        System.out.println("Empleados por separado;");
+        System.out.printf("$s\n%s: $%,.2f\n\n", empleadoAsalariado, "Ingersos ", empleadoAsalariado.ingresos());
+        System.out.printf("$s\n%s: $%,.2f\n\n", empleadoBaseMasComision, "Ingresos", empleadoBaseMasComision.ingresos());
+        System.out.printf("$s\n%s: $%,.2f\n\n", empleadoBaseMasComision, "Ingresos", empleadoBaseMasComision.ingresos());
+        Empleado[] empleado = new Empleado[4];
+        empleado[0] = empleadoAsalariado;
     }
-    
-
+}
